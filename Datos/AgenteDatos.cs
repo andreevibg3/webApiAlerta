51b51ccf7bf28b97453419c35b489386ed4271bc
Andrea Benalcázar <andrea_evelin@hotmail.com>
2019-05-18 05:01:05 -0500

    insertar y obtener registro en bdd servicio rest

---
+++ Datos/AgenteDatos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+
+namespace Datos
+{
+    public class AgenteDatos
+    {
+        BaseAlertaEntities contexto;
+        public AgenteDatos()
+        {
+            contexto = new BaseAlertaEntities();
+            contexto.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public AGENTE SeleccionarAgentePorUsuario(string usuario)
+        {
+            AGENTE respuesta = contexto.AGENTE.Where(x => x.usuario == usuario).FirstOrDefault();
+            return respuesta;
+        }
+
+        public List<AGENTE> SeleccionarAgentes()
+        {
+            List<AGENTE> respuesta = contexto.AGENTE.ToList();
+            return respuesta;
+        }
+
+        public bool Insertar(AGENTE insertado)
+        {
+            contexto.AGENTE.Add(insertado);
+            contexto.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ webApiAlerta/Controllers/AgenteController.cs
@@ -0,0 +1,50 @@
+﻿using AccesoDatos;
+using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webApiAlerta.Controllers
+{
+    public class AgenteController : ApiController
+    {
+        AgenteDatos a = new AgenteDatos();
+        // GET api/values
+        public List<AGENTE> Get()
+        {
+            return a.SeleccionarAgentes();
+        }
+
+        // GET api/values/5
+        public AGENTE Get(string usuario)
+        {
+            return a.SeleccionarAgentePorUsuario(usuario);
+        }
+
+        // POST api/values
+        public Respuesta Post([FromBody]AGENTE value)
+        {
+            Respuesta resp = new Respuesta();
+            if (a.Insertar(value))
+                 resp.valor="si valio";
+            else
+            {
+                resp.valor = "no valio";
+            }
+            return resp;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
